dda14de59c430a19eeef1b71971a62ae9a69c82c
David Vera Castillo <indigodavid@gmail.com>
2022-10-11 15:49:38 -0500

    Add nested route for expenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories
+  resources :categories do
+    resources :payments, only: :index
+  end
   resources :payments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
