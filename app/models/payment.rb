2b5a4366c4e3e8970d398c3b7ae724e609a972e9
David Vera Castillo <indigodavid@gmail.com>
2022-10-11 08:49:27 -0500

    Add validations to payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,9 @@
 class Payment < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
+
+  validates :name, :amount, presence: true
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
+  validates_associated :categories
+  validates_size_of :categories, minimum: 1
 end
