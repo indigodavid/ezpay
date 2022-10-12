6911a5df30c1d614169677298baf309daa5b8579
David Vera Castillo <indigodavid@gmail.com>
2022-10-11 22:57:19 -0500

    Install and setup factory_bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
