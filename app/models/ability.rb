4586638b875a58bf7b754049ff2de3ec6db99f3e
David Vera Castillo <indigodavid@gmail.com>
2022-10-14 16:07:00 -0500

    Fix cancancan unexistent resource

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    can :read, Splash, :all
+
     return unless user.present?
 
     can :manage, Category, user: user
