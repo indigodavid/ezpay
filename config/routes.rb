c5ab121b40e50a4fc7c2ac8302054ceb2009bfd8
David Vera Castillo <indigodavid@gmail.com>
2022-10-13 23:38:06 -0500

    Fix nested routes to follow project requirements

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'splash/index'
   devise_for :users
   resources :categories do
-    resources :payments, only: :index
+    resources :payments, only: [:index, :new]
   end
-  resources :payments
+  resources :payments, only: [:show, :edit, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
